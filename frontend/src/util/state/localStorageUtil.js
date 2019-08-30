5f233c97ebb3120d908a6587162ad44df5ec453c
Sergio <sguzman@truora.com>
2019-08-30 07:33:36 -0500

    Added error prevention for local storage in token with console.error

--- frontend/src/util/state/localStorageUtil.js
+++ frontend/src/util/state/localStorageUtil.js
@@ -2,43 +2,47 @@ const auth = {
   token: null,
   tokenTimeout: null,
   user: null,
-  refreshToken: null,
-}
+  refreshToken: null
+};
 
 export const saveAuth = authT => {
-  const timeout = authT.tokenTimeout
-  const dateEnd = new Date(new Date().getTime() + timeout * 60 * 60 * 1000)
-  authT.tokenTimeout = dateEnd
+  const timeout = authT.tokenTimeout;
+  const dateEnd = new Date(new Date().getTime() + timeout * 60 * 60 * 1000);
+  authT.tokenTimeout = dateEnd;
 
   // eslint-disable-next-line no-unused-vars
   for (const index in authT) {
-    localStorage.setItem(index, JSON.stringify(authT[index]))
+    localStorage.setItem(index, JSON.stringify(authT[index]));
   }
-}
+};
 
 export const loadAuthFromLS = () => {
-  auth.token = localStorage.getItem('token')
+  auth.token = localStorage.getItem("token");
   if (auth.token) {
     // eslint-disable-next-line no-unused-vars
     for (const index in auth) {
-      auth[index] = JSON.parse(localStorage.getItem(index))
+      try {
+        auth[index] = JSON.parse(localStorage.getItem(index));
+      } catch (e) {
+        console.error(e);
+      }
     }
     if (auth.tokenTimeout < new Date()) {
       // eslint-disable-next-line no-unused-vars
       for (const index in auth) {
-        auth[index] = null
-        localStorage.removeItem(index)
+        auth[index] = null;
+        localStorage.removeItem(index);
       }
     }
   } else {
-    auth.token = null
+    auth.token = null;
   }
-  return auth
-}
+  return auth;
+};
 
 export const logout = () => {
   // eslint-disable-next-line no-unused-vars
   for (const index in auth) {
-    localStorage.removeItem(index)
+    localStorage.removeItem(index);
   }
-}
+};
