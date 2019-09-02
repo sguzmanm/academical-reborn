c3bba43d618f81bab85f4c507d3ec48f2cb509bc
SERGIO GUZMAN MAYORGA <s.guzmanm@sis.virtual.uniandes.edu.co>
2019-09-02 10:47:15 -0500

    Added styling to events at sidebar with overflow y and random colors

--- frontend/src/components/schedule/side/Side.js
+++ frontend/src/components/schedule/side/Side.js
@@ -5,19 +5,24 @@ import { useSelector, useDispatch } from 'react-redux'
 
 function Side() {
 
+	const approvedColors=["#225378","#1595A3","#ACF0F2","#F2FFE3","#EB7F00"]
 	const events= useSelector(state=>state.events.events)
 	const testEvents=events.map(el=>(
-		<li key={el._id}>
-			{el.title+' '+el.description}
-		</li>
+		<div key={el._id} className="occurrence-cell" style={{backgroundColor:approvedColors[Math.floor(Math.random()*approvedColors.length)]}}>
+			<p>{}</p>
+			<h4>{el.title}</h4>
+			<h6>{el.type}</h6>
+		</div>
 		)
 	)
 
     return (
       <div className="side">
-		<ul>
-			{testEvents}
-		</ul>
+		<div className="search-bar">
+			Eventos
+		</div>
+			
+		{testEvents}
       </div>
     )
   }
