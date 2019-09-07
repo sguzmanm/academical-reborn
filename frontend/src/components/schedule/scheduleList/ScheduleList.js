8dd0c41ce5083760e2346c57583feeee0d329765
Checho <s.guzmanm98@gmail.com>
2019-09-06 23:56:29 -0500

    Attempt to add plus and trash icons

--- frontend/src/components/schedule/scheduleList/ScheduleList.js
+++ frontend/src/components/schedule/scheduleList/ScheduleList.js
@@ -15,6 +15,7 @@ function ScheduleList() {
 
     const mapScheduleList=mySchedules.map((el,index)=>(
         <div className={`scheduleList__item scheduleList__item${selected===index?"--selected":""}`} key={index} onClick={()=>setSelectedSchedule(index)}>
+            <button className="scheduleList__item__delete"><img src={require('../../../assets/icons/delete.svg')}/></button>
             <h2 className="scheduleList__item__title">{el.title}</h2>
             <p className="scheduleList__item__description">{el.description}</p>
             <span className="scheduleList__item__arrow">&gt;</span>
@@ -23,6 +24,9 @@ function ScheduleList() {
 
   return (
     <div className="scheduleList">
+
+      <button className="scheduleList__add"><img src={require('../../../assets/icons/add.svg')} alt="Add new schedule"/></button>
+
       {mapScheduleList}
     </div>
   )
