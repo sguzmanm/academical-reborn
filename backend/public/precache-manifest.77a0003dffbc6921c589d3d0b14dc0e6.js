844e56a0a967095ddf431f5c86f18f45ba4b43ef
Checho <s.guzmanm98@gmail.com>
2019-09-10 23:29:37 -0500

    Heroku setup?

---
+++ backend/public/precache-manifest.77a0003dffbc6921c589d3d0b14dc0e6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584e9eff3460e55a3852cd8f54f5ea1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46be2d6f49cc536565e9",
+    "url": "/static/css/2.ce41e2b0.chunk.css"
+  },
+  {
+    "revision": "40a79ffad67d08aba3ee",
+    "url": "/static/css/main.a6446363.chunk.css"
+  },
+  {
+    "revision": "46be2d6f49cc536565e9",
+    "url": "/static/js/2.2a70902f.chunk.js"
+  },
+  {
+    "revision": "40a79ffad67d08aba3ee",
+    "url": "/static/js/main.da4c8983.chunk.js"
+  },
+  {
+    "revision": "9f81d820d371aa2eb2a5",
+    "url": "/static/js/runtime~main.ca56be93.js"
+  },
+  {
+    "revision": "c19103e9090030e3e9b0e66aee2089ab",
+    "url": "/static/media/add.c19103e9.svg"
+  },
+  {
+    "revision": "f0b3c20ea10e39ad873269e5eee21b0d",
+    "url": "/static/media/delete-red.f0b3c20e.svg"
+  },
+  {
+    "revision": "5ab4c16fe9e6485c5c1d910301e20535",
+    "url": "/static/media/delete.5ab4c16f.svg"
+  },
+  {
+    "revision": "364d9886615e90afa1ece73d2f99dd4a",
+    "url": "/static/media/help.364d9886.svg"
+  },
+  {
+    "revision": "cbd8b5a8a02328308f76296212fe3d72",
+    "url": "/static/media/list.cbd8b5a8.svg"
+  },
+  {
+    "revision": "a87eca9ea0f39475f2fc7613a3af312f",
+    "url": "/static/media/magnifying-glass.a87eca9e.svg"
+  },
+  {
+    "revision": "0503f37e4812e6948cf67bfc581bb5ac",
+    "url": "/static/media/person.0503f37e.svg"
+  }
+]);
