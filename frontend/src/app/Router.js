b5ef7489d728479193242873f8f46916936a22d7
Checho <s.guzmanm98@gmail.com>
2019-09-14 18:13:27 -0500

    Fallback URL

--- frontend/src/app/Router.js
+++ frontend/src/app/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Login from "../pages/login/Login";
 import Schedule from "../pages/schedule/Schedule";
@@ -10,10 +10,12 @@ function AppRouter() {
   const token = useSelector(state => state.auth.token);
   return (
     <Router>
-      <Route path="/" exact component={Tutorial} />
-      <Route path="/login" render={() => token? <Redirect to="/schedule"/> : <Login></Login> } />
-      <Route path="/schedule" render={() => token?  <Schedule></Schedule>: <Redirect to="/login"/> } />
-      <Route path="*" render={() => <Redirect to="/"/> } />
+      <Switch>
+        <Route path="/" exact component={Tutorial} />
+        <Route path="/login" render={() => token? <Redirect to="/schedule"/> : <Login></Login> } />
+        <Route path="/schedule" render={() => token?  <Schedule></Schedule>: <Redirect to="/login"/> } />      
+        <Route render={() => token?  <Schedule></Schedule>: <Redirect to="/login"/> } />      
+      </Switch>
     </Router>
   );
 }
